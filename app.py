611d65f7af9ff2b2c7b6a4ad3b69628799df10b9
Motoki Nagano <motoki@mohantu-portable.mohapi.dip.jp>
2015-07-05 15:58:05 +0900

    キーワード検索機能を実装

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from bottle import route, run, template
+from bottle import route, run, template, request
 import requests
 
 @route('/events')
 def events():
-    r = requests.get('http://connpass.com/api/v1/event/?keyword=akashi')
+    r = requests.get('http://connpass.com/api/v1/event/?keyword=' + request.query.keyword)
     events = r.json()['events']
-
     return template('events', events=events)
-
 
 run(host='localhost', port=8080, debug=True, reloader=True)
